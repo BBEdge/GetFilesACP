0649e48fa191816ed56b152825137bc99aabdb87
BBEdge <kryamov@mail.ru>
2020-07-10 15:21:59 +0300

    Initial commit

--- sshowsys.py
+++ sshowsys.py
@@ -3,21 +3,30 @@ import gzip
 
 
 class SshowSys:
-
+    global fid
     def parce_sshowsys(item):
         skip = True
 
         with gzip.open(item[3], 'rt', encoding='utf8', errors='ignore') as f:
             for line in f:
-                switchshow = re.search(r'(\w+ [\/\w]+switchshow)', line)
+                uline = line.strip()
+                words = uline.split()
+                switchshow = re.search(r'(\w+ [\/\w]+switchshow)', uline)
                 if skip:
                     if switchshow:
                         skip = False
                 else:
                     '''parcing here'''
-                    print(line)
+                    ''' find FID '''
+                    fid = re.search(r'FID\:\s(\d+)', uline)
+                    if fid:
+                        fid = fid.group(1)
+                    ''' make Director '''
+                    director = re.search(r'Index.(?:Slot)', uline)
+                    if director:
+                        print(item[0], fid, "Director")
 
                     '''end parcing'''
-                    end = re.search(r'([*]{2} \w+ \w+ \w+ [*]{2})', line)
+                    end = re.search(r'([*]{2} \w+ \w+ \w+ [*]{2})', uline)
                     if end:
                         skip = True
